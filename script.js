479c51bb171c65504bdfaaa478e7dbf6602fed46
kid782 <stefanbeljin@gmail.com>
2021-10-21 11:58:32 +0200

    Finished all logic for calculator

--- script.js
+++ script.js
@@ -4,26 +4,36 @@ const lowerScreen=document.querySelector(".lower-screen");
 const clearScreen=document.querySelector(".clear");
 const operators=document.querySelectorAll(".operator");
 const equals=document.querySelector(".equal");
+const deleteNumber=document.querySelector(".delete");
+lowerScreen.textContent="0";
 let number1=0;
 let number2=0;
 let operatorSign="";
 
-
+//pushes first number to screen
 numbers.forEach(number=>number.addEventListener("click",()=>{
       if(lowerScreen.textContent=="Error, cant divide by 0")
       {
           return;
       }
+      else if(lowerScreen.textContent==0)
+      {
+          lowerScreen.textContent=number.textContent;
+      }
       else {
       lowerScreen.textContent+=number.textContent;
       }
 }))
+
 clearScreen.addEventListener("click",()=>
-    {lowerScreen.textContent=""
+    {lowerScreen.textContent="0"
     upperScreen.textContent="";
     defaultState();
     })
 
+deleteNumber.addEventListener("click",()=> deleteLastNumber());
+
+//gets first number and operator
 operators.forEach(operator=>operator.addEventListener("click",()=>{
     let str=lowerScreen.textContent;
     number1=Number(str);
@@ -32,9 +42,10 @@ operators.forEach(operator=>operator.addEventListener("click",()=>{
     lowerScreen.textContent="";
 }))
 
-equals.addEventListener("click",()=>
-{if(operatorSign)        
-{getResult();}})
+equals.addEventListener("click",()=>{if(operatorSign)
+                                    {
+                                        getResult();
+                                    }})
 
 function add(a,b) 
 {
@@ -81,7 +92,6 @@ function operate(num1,num2,sign)
 
 function getResult()
 {
-    upperScreen.textContent+=lowerScreen.textContent;
     let str=lowerScreen.textContent; 
     number2=Number(str);
     let res = operate(number1,number2,operatorSign);
@@ -89,15 +99,18 @@ function getResult()
         if(operatorSign=="/" && number2==0)
         {
             lowerScreen.textContent="Error, cant divide by 0";
+            upperScreen.textContent+=` ${str} = ${res}`;
             defaultState();
         }
         else if(res%1!=0)
         {
         lowerScreen.textContent=res.toFixed(2); 
+        upperScreen.textContent+=` ${str} = ${res}`;
         defaultState();
         }
         else {
         lowerScreen.textContent=res;
+        upperScreen.textContent+=` ${str} = ${res}`;
         defaultState();
         }
      
@@ -108,4 +121,19 @@ function defaultState()
     number1=0;
     number2=0;
     operatorSign="";  
+}
+
+function deleteLastNumber()
+{
+    let str=lowerScreen.textContent;
+    console.log(str);
+    console.log(typeof str);
+    let newStr=str.substring(0,str.length-1);
+    if(newStr.length)
+    {
+    lowerScreen.textContent=newStr;
+    }
+    else {
+        lowerScreen.textContent=0;
+    }
 }
